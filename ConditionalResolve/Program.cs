c4f6c6294f3d124a3307b288fe1f94bd781e15f4
Lindsay Spencer <Lindsay.Spencer@logistyx.com>
2021-01-05 18:40:37 -0600

    Resolving Dependancies conditionally.

---
+++ ConditionalResolve/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConditionalResolve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var collection = new ServiceCollection();
+
+            collection.AddScoped<EuropeTaxCalculator>();
+            collection.AddScoped<AustraliaTaxCalculator>();
+
+            collection.AddScoped<Func<UserLocations, ITaxCalculator>>(
+                ServiceProvider => key =>
+                {
+                    switch (key)
+                    {
+                        case UserLocations.Australia: return ServiceProvider.GetService<AustraliaTaxCalculator>();
+                        case UserLocations.Europe: return ServiceProvider.GetService<EuropeTaxCalculator>();
+                        default: return null;
+                    }
+                });
+
+            collection.AddSingleton<Purchase>();
+
+            var provider = collection.BuildServiceProvider();
+
+            var purchase = provider.GetService<Purchase>();
+            var totalCharge = purchase.Checkout(UserLocations.Australia);
+
+            Console.WriteLine(totalCharge);
+        }
+    }
+}
---
+++ ConditionalResolve/Purchase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConditionalResolve;
+
+namespace ConditionalResolve
+{
+    class Purchase
+    {
+        private readonly Func<UserLocations, ITaxCalculator> _accessor;
+        public Purchase(Func<UserLocations, ITaxCalculator> accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public int Checkout(UserLocations location)
+        {
+            var tax = _accessor(UserLocations.Australia);
+            var total = tax.Calculate() + 100;
+            return total;
+        }
+    }
+}
